eaf8f04135ab30fb318a8439f4237a80dbf8a21d
wahyu93 <wahyu.wahyudi1005@gmail.com>
2025-08-23 12:32:18 +0800

    section help other

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,7 +71,7 @@
         <section class="container hero">
             <div class="row align-items-center h-100">
                 <div class="col-12 col-lg-6">
-                   <h1>Welcome...<br>BeeProject Forum</h1>
+                   <h1>Welcome to <br>BeeProject Forum</h1>
                     <p class="mb-4">
                        Empowering Developers to Build and Grow with Laravel
                     </p>
@@ -79,7 +79,7 @@
                     <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Join Discussions</a>
                 </div>
                 <div class="col-12 col-lg-6 h-315px order-first order-lg-last mb-3 mb-lg-0">
-                    <img class="hero-image float-lg-end" src="{{ url('assets/images/hero1.webp') }}" alt="">
+                    <img class="hero-image float-lg-end" src="{{ url('assets/images/hero2.png') }}" alt="">
                 </div>
             </div>
         </section>
@@ -95,7 +95,7 @@
 
                 <div class="col-12 col-lg-4 text-center">
                     <img class="mb-2 promote-icon" src="{{ url('assets/images/reply.png') }}" />
-                    <h2 class="fw-bold">Replies</h2>
+                    <h2 class="fw-bold">Answers</h2>
                     <p class="fs-3">53.674</p>
                 </div>
 
@@ -103,6 +103,129 @@
                     <img class="mb-2 promote-icon" src="{{ url('assets/images/users.png') }}" />
                     <h2 class="fw-bold">Users</h2>
                     <p class="fs-3">66.347</p>
+                </div>
+            </div>
+        </section>
+
+        {{-- comment section --}}
+        <section class="bg-gray">
+            <div class="container py-80px">
+                <h2 class="text-center mb-5">Help Others</h2>
+                
+                <div class="row">
+                    {{-- comment 1 --}}
+                    <div class="col-12 col-lg-4 mb-3">
+                        <div class="card">
+                            <a href="#">
+                                <h3>Membuat Validasi di Laravel?</h3>
+                            </a>
+    
+                            <div>
+                                <p class="mb-5">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil, reprehenderit reiciendis. Temporibus sit omnis minus ea mollitia nemo iure architecto.
+                                </p>
+    
+                                <div class="row">
+                                    <div class="col me-1 me-lg-2">
+                                        <a href="#">
+                                            <span class="badge rounded-fill text-bg-light">Laravel</span>
+                                        </a>
+                                    </div>
+    
+                                    <div class="col-5 col-lg-7">
+                                        <div class="avatar-sm-wrapper d-inline-block">
+                                            <a href="" class="me-1">
+                                                <img src="{{ url('assets/images/avatar-dummy.webp') }}" class="avatar rounded-circle">
+                                            </a>
+                                        </div>
+    
+                                        <span class="fs-12px">
+                                            <a href="" class="me-1 fw-bold">Wahyu</a>
+                                            <span class="color-gray">
+                                                10 Hours Ago
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    {{-- comment 2 --}}
+                    <div class="col-12 col-lg-4 mb-3">
+                        <div class="card">
+                            <a href="#">
+                                <h3>Membuat Rumus Sederhana Diexcel?</h3>
+                            </a>
+    
+                            <div>
+                                <p class="mb-5">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi ipsa est voluptate quaerat ea repellat quia. Dolorum animi illo officiis.
+                                </p>
+    
+                                <div class="row">
+                                    <div class="col me-1 me-lg-2">
+                                        <a href="#">
+                                            <span class="badge rounded-fill text-bg-light">Laravel</span>
+                                        </a>
+                                    </div>
+    
+                                    <div class="col-5 col-lg-7">
+                                        <div class="avatar-sm-wrapper d-inline-block">
+                                            <a href="" class="me-1">
+                                                <img src="{{ url('assets/images/avatar-dummy.webp') }}" class="avatar rounded-circle">
+                                            </a>
+                                        </div>
+    
+                                        <span class="fs-12px">
+                                            <a href="" class="me-1 fw-bold">Wahyu</a>
+                                            <span class="color-gray">
+                                                10 Hours Ago
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    
+                    {{-- comment 3 --}}
+                    <div class="col-12 col-lg-4 mb-3">
+                        <div class="card">
+                            <a href="#">
+                                <h3>Lorem ipsum dolor sit, amet consectetur adipisicing?</h3>
+                            </a>
+    
+                            <div>
+                                <p class="mb-5">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt dignissimos id enim ab placeat iste exercitationem officia possimus, porro quasi.
+                                </p>
+    
+                                <div class="row">
+                                    <div class="col me-1 me-lg-2">
+                                        <a href="#">
+                                            <span class="badge rounded-fill text-bg-light">Laravel</span>
+                                        </a>
+                                    </div>
+    
+                                    <div class="col-5 col-lg-7">
+                                        <div class="avatar-sm-wrapper d-inline-block">
+                                            <a href="" class="me-1">
+                                                <img src="{{ url('assets/images/avatar-dummy.webp') }}" class="avatar rounded-circle">
+                                            </a>
+                                        </div>
+    
+                                        <span class="fs-12px">
+                                            <a href="" class="me-1 fw-bold">Wahyu</a>
+                                            <span class="color-gray">
+                                                10 Hours Ago
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
